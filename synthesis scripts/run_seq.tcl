186c147dd2cf09891acab9ba14e4d457070d052a
changcc656-oss <changcc656@gmail.com>
2026-07-12 19:34:41 +0800

    Update run_seq.tcl

--- synthesis scripts/run_seq.tcl
+++ synthesis scripts/run_seq.tcl
@@ -12,7 +12,5 @@ compile_ultra
 redirect -file reports/seq_area.rpt   { report_area }
 redirect -file reports/seq_timing.rpt { report_timing -delay max -max_paths 10 }
 redirect -file reports/seq_power.rpt  { report_power }
-redirect -file reports/seq_clock.rpt  { report_clock }
-redirect -file reports/seq_qor.rpt { report_qor }
 
 quit
