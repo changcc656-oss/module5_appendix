e3f8dfb8b806de9f2c297405a44ccc7ced386b66
changcc656-oss <changcc656@gmail.com>
2026-07-12 19:34:33 +0800

    Update run_comb.tcl

--- synthesis scripts/run_comb.tcl
+++ synthesis scripts/run_comb.tcl
@@ -11,7 +11,5 @@ compile_ultra
 redirect -file reports/comb_area.rpt   { report_area }
 redirect -file reports/comb_timing.rpt { report_timing -delay max -max_paths 10 }
 redirect -file reports/comb_power.rpt  { report_power }
-redirect -file reports/comb_clock.rpt  { report_clock }
-redirect -file reports/comb_qor.rpt { report_qor }
 
 quit
